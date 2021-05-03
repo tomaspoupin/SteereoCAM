d8ebde0091534fde8f76ee38091a8d3b8b59720e
Tomas <tomas.mpoupin@gmail.com>
2021-05-03 11:40:33 -0400

    Se arreglaron detalles en script de medicion

--- depth_precision/depth_measurement.cpp
+++ depth_precision/depth_measurement.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <commons.hpp>
 #include <CSVWriter.h>
+#include <sstream>
 
 cv::Mat generateInfMask(cv::Mat src);
 
@@ -9,9 +10,27 @@ const ACCURACY_MODE accuracy_mode[3] = {ACCURACY_MODE::LOW,
                                         ACCURACY_MODE::HIGH,
                                         ACCURACY_MODE::ULTRA};
 
-double time_ms = 60000;
+double time_ms = 0;
 
 int main(int argc, char** argv) {
+
+    if (argc != 3) {
+        std::cerr << "Uso: benchmark_depth_sdk <tiempo en segundos>"
+                     " <depth accuracy>" << std::endl;
+        return 1;
+    }
+
+    int accuracy;
+    std::stringstream buffer;
+    buffer << argv[1];
+    buffer >> time_ms;
+    time_ms *= 1000.0;
+
+    buffer.str("");
+    buffer.clear();
+    buffer << argv[2];
+    buffer >> accuracy;
+
     TaraXL taraxl_cam;
     TaraXLCam selected_cam;
     TaraXLCamList taraxl_cam_list;
@@ -39,27 +58,41 @@ int main(int argc, char** argv) {
 
     cv::Rect ROI(700, 550, 200, 200);
     
-    taraxl_depth->setAccuracy(accuracy_mode[1]);
+    taraxl_depth->setAccuracy(accuracy_mode[accuracy]);
 
     for (int i = 0; i < 10; ++i)
         taraxl_depth->getMap(left, right, disparity_map, true, depth_map, true);
 
-    auto init = std::chrono::high_resolution_clock::now();
+    cv::Mat depthU8, depthROI(depth_map, ROI);
+    cv::normalize(depth_map, depthU8, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+
+    cv::imwrite("left.jpg", left);
+    cv::imwrite("right.jpg", left);
+    cv::imwrite("ROI.jpg", depthROI);
+    cv::imwrite("depth_map.jpg", depth_map);
+
+    std::cout << "Modo de medicion: " << ac_string[accuracy] << std::endl;
+    std::cout << "Comenzando medicion, tiempo: " << (int) time_ms / 1000 << " segundos." << std::endl;
+
+    auto init_time = std::chrono::high_resolution_clock::now();
     while(true){
         auto checkpoint = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double, std::milli> ms_checkpoint = checkpoint - init;
+        std::chrono::duration<double, std::milli> ms_checkpoint = checkpoint - init_time;
         if (ms_checkpoint.count() >= time_ms)
             break;
 
         taraxl_depth->getMap(left, right, disparity_map, true, depth_map, true);
         cv::Mat measurement_area(depth_map, ROI);
         cv::Mat mask = generateInfMask(measurement_area);
-        cv::Scalar mean = cv::mean(area, mask);
+        cv::Scalar mean = cv::mean(measurement_area, mask);
         measurements.newRow() << mean.val[0];
     }
+
+    std::cout << "Medición finalizada, exportando archivo." << std::endl;
     
     measurements.writeToFile("distancias.csv");
 
+    std::cout << "Archivo exportado." << std::endl;
     selected_cam.disconnect();
 }
 
